1f3fae4e34424d51f12f101842296aaf0cc990c1
Shahmin Rahman Rinkey <rinkeyrahman@gmail.com>
2018-11-26 11:02:40 +0600

    implement video processing

---
+++ video_face_recognition.py
@@ -0,0 +1,1 @@
+#test face recognition model on video frames
--- video_processing.py
+++ video_processing.py
@@ -1,1 +1,23 @@
 #video processing
+import cv2
+import math
+
+
+def video_process():
+    count = 0
+    video_file = "Before Sunrise Trailer.mp4"
+    capture = cv2.VideoCapture(video_file)
+    frameRate = capture.get(5)
+    x=1
+    while(capture.isOpened()):
+        frame_id = capture.get(1)
+        ret, frame = capture.read()
+        if (ret != True):
+            break
+        if (frame_id % math.floor(frameRate) == 0):
+            filename ="frame%d.jpg" % count;count+=1
+            cv2.imwrite('images/' + filename, frame)
+
+    capture.release()
+
+video_process()
